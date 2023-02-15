a70b8b9c722e7e19385f6512fcc746dbd2505676
codingfabi <fabian.kneissl@gmx.de>
2023-02-15 09:22:27 +0100

    add missing docstrings (#38)

--- hetpy/exceptions/typeExceptions.py
+++ hetpy/exceptions/typeExceptions.py
@@ -1,6 +1,6 @@
 class TypeException(Exception):
     """
-    TODO: Add docstrings
+    An exception to throw when the type of some object is undefined or invalid.
     """
     def __init__(self, details):            
         # Call the base class constructor with the parameters it needs
